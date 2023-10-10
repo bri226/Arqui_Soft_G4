d31a36dbb8a5f8aed5b9ddd8cf0d670ca6f72a8b
Alexander Cruz <amatu85@gmail.com>
2023-10-10 01:49:25 -0500

    Revert "Moviendo contenido del submódulo al repositorio principal"
    This reverts commit 4c643e124c11b1923fec5fd41c102f6c3f60fd35.

--- src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/index.controller');
-
-
-router.get('/', controller.index);
-
-
-module.exports = { route: router };
